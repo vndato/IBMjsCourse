25d5f6b64a61f12983adcb7c3576dbf57752f860
vndato <vinhdattruong1309@gmail.com>
2024-04-13 00:19:40 +0200

    Added second lab files

---
+++ SampleFolder/Scope/scope_lab.js
@@ -0,0 +1,22 @@
+// Global scope
+var globalVar = "I'm a global varibale";
+let globalLet = "I'm also a global, but scope with let";
+const globalConst = "I'm a global constant";
+
+// Block scope
+{
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
